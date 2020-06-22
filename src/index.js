fe7e7fd7c0a1c0764acc5b6c46662bee33d6a9ab
Josh Burrill <joshua.burrill.1@gmail.com>
2020-06-22 17:34:44 +0100

    Updated redux persist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 import { store, persistor } from "./redux/store";
 
